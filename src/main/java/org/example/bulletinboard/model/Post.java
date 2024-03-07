efc2610d816342d661ce13002317dad8f1233a42
S-DPR <thankyouformuch@naver.com>
2024-03-07 23:13:56 +0900

    feat: JPA config 설정, 날짜 및 id 자동입력으로 변경

--- src/main/java/org/example/bulletinboard/model/Post.java
+++ src/main/java/org/example/bulletinboard/model/Post.java
@@ -1,20 +1,26 @@
 package org.example.bulletinboard.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 
 @Setter
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String title;
     private String content;
+    @CreatedDate
     private Timestamp createTime;
+    @LastModifiedDate
     private Timestamp editTime;
 }
