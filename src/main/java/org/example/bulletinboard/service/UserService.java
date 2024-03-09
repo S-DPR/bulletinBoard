d8ea05fd7774c9ed2c31aa107d385fbfafc27860
S-DPR <thankyouformuch@naver.com>
2024-03-09 17:00:13 +0900

    feat: userService에 유저 저장, 로그인 비밀번호 매칭 구현

--- src/main/java/org/example/bulletinboard/service/UserService.java
+++ src/main/java/org/example/bulletinboard/service/UserService.java
@@ -1,7 +1,29 @@
 package org.example.bulletinboard.service;
 
+import org.example.bulletinboard.model.User;
+import org.example.bulletinboard.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean login(User user) {
+        return userRepository.findById(user.getId())
+                .map(findUser -> findUser.getPassword().equals(user.getPassword()))
+                .orElseGet(() -> {
+                    save(user); // 빠른 기능 구현을 위해 회원가입 기능 일단은 제외
+                    return true;
+                });
+    }
 }
