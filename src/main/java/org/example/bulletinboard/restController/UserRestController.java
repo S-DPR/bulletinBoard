7cfbf521c68fd69c2ac9ca38f5fc837bb73d3461
S-DPR <thankyouformuch@naver.com>
2024-03-09 17:00:37 +0900

    feat: UserRestController에 login, save기능 구현

--- src/main/java/org/example/bulletinboard/restController/UserRestController.java
+++ src/main/java/org/example/bulletinboard/restController/UserRestController.java
@@ -1,9 +1,31 @@
 package org.example.bulletinboard.restController;
 
+import org.example.bulletinboard.model.User;
+import org.example.bulletinboard.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
-public class UserRestController {
+@RequestMapping("/sys/login")
+public class UserRestController extends GenericRestController {
+    private final UserService userService;
+
+    @Autowired
+    UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity login(@ModelAttribute User user) {
+        return response(userService.login(user));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity save(@ModelAttribute User user) {
+        return response(userService.save(user));
+    }
 }
