7bc1e0c14f8b64c6fe68b694c4dceac5c8fc0b57
S-DPR <thankyouformuch@naver.com>
2024-03-10 17:46:18 +0900

    feat: FilterDTO를 사용한 포스트 Query 추가

--- src/main/java/org/example/bulletinboard/repos/PostRepository.java
+++ src/main/java/org/example/bulletinboard/repos/PostRepository.java
@@ -1,9 +1,22 @@
 package org.example.bulletinboard.repos;
 
 import org.example.bulletinboard.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, String> {
+    @Query("SELECT p FROM Post p " +
+            "WHERE (:title IS NULL OR p.title LIKE %:title%) " +
+            "AND (:content IS NULL OR p.content LIKE %:content%) " +
+            "AND (:writer IS NULL OR p.writer = :writer)")
+    Page<Post> findByFilter(@Param("title") String title,
+                            @Param("content") String content,
+                            @Param("writer") String writer,
+                            Pageable pageable);
 }
+
