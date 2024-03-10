9d15cea2b6d8b3c8d362583d26435ae9111247f6
S-DPR <thankyouformuch@naver.com>
2024-03-10 20:13:23 +0900

    refactoring: 메뉴로 돌아가는 버튼 제거

--- src/main/resources/static/js/read.js
+++ src/main/resources/static/js/read.js
@@ -1,12 +1,8 @@
 window.addEventListener('load', () => {
     const postId = document.querySelector('#id').value
-    const mainBtn = document.querySelector('#mainBtn')
     const editBtn = document.querySelector('#editBtn')
     const delBtn = document.querySelector('#delBtn')
 
-    mainBtn.addEventListener('click', () => {
-        location.href = `/`
-    })
     if (editBtn) {
         editBtn.addEventListener('click', () => {
             location.href = `/postWrite/${postId}`
